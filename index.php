f913ae2d9d9c8d39c3c1c76f8ec6fcd9bc940c96
sarcy  nenepat <82246009+sarcy29@users.noreply.github.com>
2022-03-19 09:36:53 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+//loop
+
+//while loop
+//do while loop
+//for loop
+//foreach loop
+$array = array("Daniel", "Jane", "Jacob", "John", "Carlos");
+
+foreach ($array as $loopdata) {
+    echo "My name is " .$loopdata. "<br>";
+} 
+
+?>
+</body>
+</html
